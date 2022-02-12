0cc43212c1b04a6a0b407de7642dbda4c54e939f
Patrik Přidal <98656826+vibemane@users.noreply.github.com>
2022-02-12 21:34:30 +0100

    miniprofile

--- components/Post.js
+++ components/Post.js
@@ -53,6 +53,13 @@ function Post({ id, username, userImg, img, caption }) {
         {/* Comments */}
 
         {/* input box */}
+        <form className="flex items-center p-4">
+            <EmojiHappyIcon className="h-7" />
+            <input type="text" 
+            placeholder="Add a comment..."
+            className="border-none flex-1 focus:ring-0" />
+            <button className="font-semibold text-blue-400">Post</button>
+        </form>
 
     </div>
   )
